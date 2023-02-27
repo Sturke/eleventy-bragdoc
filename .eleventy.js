65a2bc0d73e07d75b0922beec506713928e700b6
Sturke <burke.stanion@gmail.com>
2023-02-27 00:37:33 -0600

    11ty working

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+   
+  return {
+      dir: {
+        input: "src",
+        includes: "_includes",
+        output: "_site"
+      },
+      templateFormats: [ 'md', 'njk', 'html'],
+      markdownTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
+    };
+    //eleventyConfig.addPassthroughCopy("css/styles.css"); 
+  }
